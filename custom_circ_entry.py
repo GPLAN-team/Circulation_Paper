30069c0fbfe5363311ed862edda89cfc712f6aa8
A.Sudarshan <f20190744@pilani.bits-pilani.ac.in>
2022-07-29 20:16:01 +0530

    found corridor tree to remove unused corridors

--- custom_circ_entry.py
+++ custom_circ_entry.py
@@ -13,11 +13,13 @@ class custom_circ:
         self.graph = graph
         self.span_circ = nx.Graph()
         self.adjacency = {}
+        self.modified_circ = nx.Graph()
+        self.corridor_tree = nx.Graph()
 
-    def custom_circ(self,f1: int, f2: int,s1: int = 1,s2: int = 2,v1: int = 1,v2: int = 2) -> nx.Graph:
+    def custom_circ(self,f1: int, f2: int,s1: int = 1,s2: int = 2,v1: int = 0,v2: int = 1) -> nx.Graph:
         """
         Modifies the circulation graph having the spanning circulation to restrict it to user choice
-        
+    
         Args:
             f1 (int, optional): First endpoint of the target edge to end the circulation (maybe interior). Defaults to 1.
             f2 (int, optional): Second endpoint of the target edge to end the circulation (maybe interior). Defaults to 2.
@@ -29,16 +31,15 @@ class custom_circ:
         # Steps involved:
         # Step 1: Find spanning circulation
         # Step 2: Find corridor vertex on the edge s1--s2 and similarly f1--f2
-        # Step 3: Change label of other corridor vertices to a large number
+        # Step 3: Contract the edges having unnecessary corridor vertices
 
         # Step 1 (Note: later call the circ_algo from circulation.py instead of repeating code)
 
         graph = deepcopy(self.graph)
-        print(nx.to_numpy_matrix(graph))
         # n is the number of vertices in the initial graph
         n = len(graph)
         m = n
-        s = (v1-1 ,v2-1 , -1)
+        s = (v1 ,v2 , -1)
 
         # This dictionary tracks the pair of rooms each corridor is adjacent to
         # (key is vertex corresponding to corridor and values are a pair of rooms)
@@ -60,7 +61,7 @@ class custom_circ:
                     except:
                         print("WARNING!! THE INITIAL CHOSEN ENTRY EDGE MUST BE EXTERIOR EDGE") # Warning displayed
                         return 0
-                    
+                
                     if s[2]>0:
                         # If condition satisfied this adds edge between current corridor vertex and previous one
                         graph.add_edge(n,s[2])
@@ -71,19 +72,12 @@ class custom_circ:
                     corridor_counter += 1
                     queue.append((ne,s[0],n-1))
                     queue.append((ne,s[1],n-1))
-        
+    
         # Change the key values to corridor vertex number
         corridor_vertices = [x+m for x in range(len(adjacency))]
 
         # Now the final_adjacency dictionary contains the pair of rooms adjacent to each corridor
         final_adjacency = dict(zip(corridor_vertices, list(adjacency.values())))
-
-        # Now A stores the adjacency matrix of the graph, the nodelist parameter ensures the proper order of rows
-        # corresponding to the node id
-        A = nx.adjacency_matrix(graph, nodelist=range(m))
-
-        # todense prints the matrix in proper format along with the data type
-        print(A.todense())
 
         self.span_circ = graph
         self.adjacency = final_adjacency
@@ -99,14 +93,93 @@ class custom_circ:
         for i in range(len(self.graph),len(self.span_circ)):
             # Gets the tuple corresponding to the key value (key = corridor_vertex = i(here))
             [a,b] = self.adjacency.get(i)
-            if([a,b] == [s1,s2]):
+            if([a,b] == [s1,s2] or [b,a] == [s1,s2]):
                 if (min > i):
-                    i = min
+                    min = i
 
-            if([a,b] == [f1,f2]):
+            if([a,b] == [f1,f2] or [b,a] == [f1,f2]):
                 if(max < i):
                     max = i
-        
+    
         # Step 3
 
-        # for i in range(min,max+1):
+        # # We collect the indices that need to be removed
+        # indices = [x for x in range(len(self.graph),min)]
+        # for i in range(max + 1, len(self.span_circ)):
+        #     indices.append(i)
+        
+        # # Contracting the edges to modify the circulation
+        # for i in indices:
+        #     [a,b] = self.adjacency.get(i)
+        #     mod_circ = nx.contracted_edge(self.span_circ, (a,i), False)
+        # self.modified_circ = mod_circ
+
+        # Trying to find shortest path:
+        self.corridor_tree = nx.induced_subgraph(self.span_circ, range(len(self.graph), len(self.span_circ)))
+
+        # Since we find simple path in a tree, we get a single path
+        path = nx.all_simple_paths(self.corridor_tree, min, max)
+
+        # We get the indices that are need to be removed from the spanning circulation
+        indices = [x for x in range(len(self.graph), len(self.span_circ))]
+
+        for i in path:
+            for idx in i:
+                indices.remove(idx)
+        
+        print(indices)
+
+        # Contracting the edges to modify the circulation
+        for i in indices:
+            [a,b] = self.adjacency.get(i)
+            mod_circ = nx.contracted_edge(self.span_circ, (a,i), False)
+        self.modified_circ = mod_circ
+    
+def plot(graph: nx.Graph,m: int) -> None:
+    """Plots thr graph using matplotlib
+
+    Args:
+        graph (Networkx graph): The graph to plot
+        m (integer): Number of vertices in the graph
+    """
+    pos=nx.spring_layout(graph) # positions for all nodes
+    nx.draw_networkx(graph,pos, label=None,node_size=400 ,node_color='#4b8bc8',font_size=12, font_color='k', font_family='sans-serif', font_weight='normal', alpha=1, bbox=None, ax=None)
+    nx.draw_networkx_edges(graph,pos)
+    nx.draw_networkx_nodes(graph,pos,
+                        nodelist=list(range(m,len(graph))),
+                        node_color='r',
+                        node_size=500,
+                    alpha=1)
+    plt.show()
+
+def main():
+    def make_graph():
+        g = nx.Graph()
+        g.add_edge(0,1)
+        g.add_edge(0,2)
+        g.add_edge(1,3)
+        g.add_edge(1,2)
+        g.add_edge(1,4)
+        g.add_edge(3,6)
+        g.add_edge(3,4)
+        g.add_edge(2,4)
+        g.add_edge(2,5)
+        g.add_edge(4,5)
+        g.add_edge(4,6)
+        g.add_edge(5,6)
+
+        return g
+
+    def test_custom_circ():
+        g = make_graph()
+        plot(g,len(g))
+        custom_obj = custom_circ(g)
+        custom_obj.custom_circ(3,4,1,2)
+        # plot(custom_obj.span_circ, len(custom_obj.span_circ))
+        # plot(custom_obj.corridor_tree, len(custom_obj.corridor_tree))
+        # plot(custom_obj.modified_circ, len(custom_obj.modified_circ))
+    
+    test_custom_circ()
+
+if __name__ == "__main__":
+    main()
