e7f50890abe762933e1e5716380ce4de449d03a7
A.Sudarshan <66859664+ASudu@users.noreply.github.com>
2023-02-16 16:04:38 +0530

    trying grids

--- test.py
+++ test.py
@@ -4,72 +4,162 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
-root = Tk()
-root.title("Test GUI GPLAN")
-root.geometry("500x400")
+# import package and making objects
+import turtle
 
-# Create a Main Frame
-main_frame = Frame(root)
-main_frame.pack(fill=BOTH, expand=1)
 
-# Create Canvas
-my_canvas = Canvas(main_frame)
-my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
+sc=turtle.Screen()
+trtl=turtle.Turtle()
 
-# Add Scrollbar to the canvas
-my_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
-my_scrollbar.pack(side=RIGHT, fill=Y)
+# method to draw y-axis lines
+def drawy(val):
+	
+	# line
+	trtl.forward(300)
+	
+	# set position
+	trtl.up()
+	trtl.setpos(val,300)
+	trtl.down()
+	
+	# another line
+	trtl.backward(300)
+	
+	# set position again
+	trtl.up()
+	trtl.setpos(val+10,0)
+	trtl.down()
+	
+# method to draw y-axis lines
+def drawx(val):
+	
+	# line
+	trtl.forward(300)
+	
+	# set position
+	trtl.up()
+	trtl.setpos(300,val)
+	trtl.down()
+	
+	# another line
+	trtl.backward(300)
+	
+	# set position again
+	trtl.up()
+	trtl.setpos(0,val+10)
+	trtl.down()
+	
+# method to label the graph grid
+def lab():
+	
+	# set position
+	trtl.penup()
+	trtl.setpos(155,155)
+	trtl.pendown()
+	
+	# write 0
+	trtl.write(0,font=("Verdana", 12, "bold"))
+	
+	# set position again
+	trtl.penup()
+	trtl.setpos(290,155)
+	trtl.pendown()
+	
+	# write x
+	trtl.write("x",font=("Verdana", 12, "bold"))
+	
+	# set position again
+	trtl.penup()
+	trtl.setpos(155,290)
+	trtl.pendown()
+	
+	# write y
+	trtl.write("y",font=("Verdana", 12, "bold"))
+	
 
-# Configure the canvas
-my_canvas.configure(yscrollcommand=my_scrollbar.set)
-my_canvas.bind('<Configure>',lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+# Main Section
+# set screen
+sc.setup(800,800)
 
-# Create another frame inside the canvas
-second_frame = Frame(my_canvas)
+# set turtle features
+trtl.speed(100)
+trtl.left(90)
+trtl.color('lightgreen')
 
-# Add that new frame to a window in the canvas
-my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+# y lines
+for i in range(30):
+	drawy(10*(i+1))
 
-corr_text = tk.Label(second_frame,text="Enter 1 if you want to remove corridor",justify=tk.CENTER).grid(row=1, column=2)
-for i in range(50):
-    Button(second_frame, text=f'Button {i}').grid(row=i, column=0, padx=10, pady=10)
+# set position for x lines
+trtl.right(90)
+trtl.up()
+trtl.setpos(0,0)
+trtl.down()
 
-root.mainloop()
-# def plot(graph: nx.Graph,m: int) -> None:
-#     """Plots thr graph using matplotlib
+# x lines
+for i in range(30):
+	drawx(10*(i+1))
 
-#     Args:
-#         graph (Networkx graph): The graph to plot
-#         m (integer): Number of vertices in the graph
-#     """
-#     pos=nx.spring_layout(graph) # positions for all nodes
-#     nx.draw_networkx(graph,pos, label=None,node_size=400 ,node_color='#4b8bc8',font_size=12, font_color='k', font_family='sans-serif', font_weight='normal', alpha=1, bbox=None, ax=None)
-#     nx.draw_networkx_edges(graph,pos)
-#     nx.draw_networkx_nodes(graph,pos,
-#                         nodelist=list(range(m,len(graph))),
-#                         node_color='r',
-#                         node_size=500,
-#                     alpha=1)
-#     plt.show()
+# axis
+trtl.color('green')
 
-# g = nx.Graph()
-# g.add_edge(0,1)
-# g.add_edge(0,2)
-# g.add_edge(1,3)
-# g.add_edge(1,2)
-# g.add_edge(1,4)
-# g.add_edge(3,6)
-# g.add_edge(3,4)
-# g.add_edge(2,4)
-# g.add_edge(2,5)
-# g.add_edge(4,5)
-# g.add_edge(4,6)
-# g.add_edge(5,6)
+# set position for x axis
+trtl.up()
+trtl.setpos(0,150)
+trtl.down()
 
-# plot(g,len(g))
-# h = nx.induced_subgraph(g,[x for x in range(5)])
-# plot(h,len(h))
+# x-axis
+# trtl.forward(300)
 
+# set position for y axis
+trtl.left(90)
+trtl.up()
+trtl.setpos(150,0)
+trtl.down()
+
+# y-axis
+# trtl.forward(300)
+
+# labeling
+# lab()
+
+# hide the turtle
+trtl.hideturtle()
+
+"""Sample code for scrollbar """
+# root = Tk()
+# root.title("Test GUI GPLAN")
+# root.geometry("500x400")
+
+# # Create a Main Frame
+# main_frame = Frame(root)
+# main_frame.pack(fill=BOTH, expand=1)
+
+# # Create Canvas
+# my_canvas = Canvas(main_frame)
+# my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
+
+# # Add Scrollbar to the canvas
+# my_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
+# my_scrollbar.pack(side=RIGHT, fill=Y)
+
+# # Configure the canvas
+# my_canvas.configure(yscrollcommand=my_scrollbar.set)
+# my_canvas.bind('<Configure>',lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+# # Create another frame inside the canvas
+# second_frame = Frame(my_canvas)
+
+# # Add that new frame to a window in the canvas
+# my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+
+# corr_text = tk.Label(second_frame,text="Enter 1 if you want to remove corridor",justify=tk.CENTER).grid(row=1, column=2)
+# for i in range(50):
+#     Button(second_frame, text=f'Button {i}').grid(row=i, column=0, padx=10, pady=10)
+
+# root.mainloop()
+
+""" Sample code to use dropdowns """
 # OZoneIzotopeSemiWhite = "#c0c4ca"
 # buttonBackground = "#303336"
 # buttonforeground = "#cdd0d7"
